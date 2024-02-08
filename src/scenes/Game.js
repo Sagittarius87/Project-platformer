329750e480b6484ddfb6666fd4589642d0e8ae75
Sagittarius87 <Jarik-87@yandex.ru>
2024-02-08 18:26:30 +0300

    В Game.js для createPlayer() добавил анимацию

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -44,7 +44,23 @@ export class Game extends Scene
         this.player.setBounce(0.2)
         this.player.setCollideWorldBounds(true)
 
-        
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers(DUDE_KEY, { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        })
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: DUDE_KEY, frame: 4 } ],
+            frameRate: 20
+        })
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers(DUDE_KEY, { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        })
     }
 
 }
