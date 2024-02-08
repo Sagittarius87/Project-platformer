3f58ad9b4e047ac9bb756f1d71e8e62485f26edb
Sagittarius87 <Jarik-87@yandex.ru>
2024-02-08 12:33:17 +0300

    В Game.js добавил константы

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,5 +1,8 @@
 import { Scene } from 'phaser';
 
+const GROUND_KEY = 'platform'
+const SKY_KEY = 'sky'
+
 export class Game extends Scene
 {
     constructor ()
@@ -9,7 +12,7 @@ export class Game extends Scene
 
     create ()
     {
-        this.add.image(512, 384, 'sky')
+        this.add.image(512, 384, SKY_KEY)
         this.createPlatforms()
         
 
@@ -24,8 +27,8 @@ export class Game extends Scene
     {
         const platforms = this.physics.add.staticGroup()
 
-        platforms.create(512, 660, 'platform').setScale(2).refreshBody()
-        platforms.create(312, 484, 'platform')
-        platforms.create(712, 350, 'platform')
+        platforms.create(512, 660, GROUND_KEY).setScale(2).refreshBody()
+        platforms.create(312, 484, GROUND_KEY)
+        platforms.create(712, 350, GROUND_KEY)
     }
 }
